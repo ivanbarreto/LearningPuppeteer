6f3480dac0b3af126ffe4eab3b22933230394032
Ivan Barreto <ivansbarreto1@gmail.com>
2019-06-25 11:13:47 +1200

    adding support for pagination

--- ycombinator-scraper.js
+++ ycombinator-scraper.js
@@ -1,21 +1,40 @@
 const puppeteer = require('puppeteer');
-function run () {
+function run (pagesToScrape) {
     return new Promise(async (resolve, reject) => {
         try {
+            if (!pagesToScrape) {
+                pagesToScrape = 1;
+            }
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
             await page.goto("https://news.ycombinator.com/");
-            let urls = await page.evaluate(() => {
-                let results = [];
-                let items = document.querySelectorAll('a.storylink');
-                items.forEach((item) => {
-                    results.push({
-                        url:  item.getAttribute('href'),
-                        text: item.innerText,
+            let currentPage = 1;
+            let urls = [];
+            //runs until the last page is scrapped
+            while (currentPage <= pagesToScrape) {
+                //gets results for each individual page
+                let newUrls = await page.evaluate(() => {
+                    let results = [];
+                    let items = document.querySelectorAll('a.storylink');
+                    items.forEach((item) => {
+                        results.push({
+                            url:  item.getAttribute('href'),
+                            text: item.innerText,
+                        });
                     });
+                    return results;
                 });
-                return results;
-            })
+                //joins the urls
+                urls = urls.concat(newUrls);
+                //after processing and joining the URLs, if we are not on the last page, clicks to access the next page
+                if (currentPage < pagesToScrape) {
+                    await Promise.all([
+                        await page.click('a.morelink'),
+                        await page.waitForSelector('a.storylink')
+                    ])
+                }
+                currentPage++;
+            }
             browser.close();
             return resolve(urls);
         } catch (e) {
@@ -23,4 +42,5 @@ function run () {
         }
     })
 }
-run().then(console.log).catch(console.error);
+//runs the script on 5 pages
+run(5).then(console.log).catch(console.error);
